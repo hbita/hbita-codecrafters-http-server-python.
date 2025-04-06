8b241b00d53c6b9672e1cf0943037bce4604a935
hbita <h.bita.8@gmail.com>
2025-04-06 17:41:18 +0200

    #3

--- app/main.py
+++ app/main.py
@@ -9,9 +9,18 @@ def main():
     connection, address = server_socket.accept()
     data = connection.recv(1024).decode()
     request_line = data.split("\n\r")[0]
-    path=request_line.split(" ")[1]
-    if path == '/':
-         response = "HTTP/1.1 200 OK\r\n\r\n"
+    method,path ,_ =request_line.split(" ",2)
+    if path.startswith("/echo/"):
+         response_str = path.split("/echo/")[1]
+         response_headers = (
+            "HTTP/1.1 200 OK\r\n"
+            f"Content-Type: text/plain\r\n"
+            f"Content-Length: {len(response_str)}\r\n"
+            "\r\n")
+         response_body = response_str
+         response = (response_headers + response_body)
+    elif path == "/":
+         response="HTTP/1.1 200 OK\r\n\r\n"
     else :
          response = "HTTP/1.1 404 Not Found\r\n\r\n"
     connection.sendall(response.encode())
