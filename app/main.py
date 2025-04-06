89bb90df9b8b5a6f2fb2cb4d2f9e23cfe20c328f
hbita <h.bita.8@gmail.com>
2025-04-06 16:56:57 +0200

    #1

--- app/main.py
+++ app/main.py
@@ -7,8 +7,9 @@ def main():
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     server_socket.accept() # wait for client
-    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-
+    connection, address = server_socket.accept()
+    data = connection.recv(1024)
+    connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 if __name__ == "__main__":
     main()
