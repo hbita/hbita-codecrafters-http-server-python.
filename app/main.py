b856244c54d66d3ff2caa76b7f1d7f28fee62174
hbita <h.bita.8@gmail.com>
2025-04-06 17:23:14 +0200

    #2

--- app/main.py
+++ app/main.py
@@ -7,10 +7,13 @@ def main():
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     connection, address = server_socket.accept()
-    data = connection.recv(1024)
-    connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-    connection.close()
-    server_socket.close()
-
+    data = connection.recv(1024).decode()
+    request_line = data.split("\n\r")[0]
+    path=request_line.split(" ")[1]
+    if path == '/':
+         response = "HTTP/1.1 200 OK\r\n\r\n"
+    else :
+         response = "HTTP/1.1 404 Not Found\r\n\r\n"
+    connection.sendall(response.encode())
 if __name__ == "__main__":
     main()
